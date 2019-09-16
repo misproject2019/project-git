c15ab3b5bb4cacf136c4334c11f0cfb91b4396f0
misproject2019 <55050561+misproject2019@users.noreply.github.com>
2019-09-16 20:19:25 +0800

    Add files via upload

--- recommend.py
+++ recommend.py
@@ -25,7 +25,6 @@ db = firebase.database()
 
 from database import DataBase
 df = DataBase("早餐早午餐中式.txt")
-
 class RecomWindow(QWidget, recommend_ui.Ui_Form):
     userid = ""
     def __init__(self, parent = None):
@@ -33,31 +32,43 @@ class RecomWindow(QWidget, recommend_ui.Ui_Form):
         self.setupUi(self)
         self.pushButton.clicked.connect(self.recommend)
         self.pushButton_2.clicked.connect(self.toMain)
+        self.checkBox.toggled.connect(lambda: self.btnstate(self.checkBox))
+        self.groupBox_2.setVisible(False)
+        self.pushButton_3.clicked.connect(self.correct)
+    def correct(self):
+        
+        self.widget.setVisible(False)
+    def btnstate(self, btn):
+        if self.checkBox.isChecked():
+            self.groupBox_2.setVisible(True)
+        else:
+            self.groupBox_2.setVisible(False)
     def recommend(self):
-        test.a="A"
-        print(test.a)
-        category = [self.checkBox, self.checkBox_2, self.checkBox_3, self.checkBox_4, self.checkBox_5, self.checkBox_6, self.checkBox_7, self.checkBox_8]
+        '''test.a="A"
+        print(test.a)'''
+        category = [self.checkBox, self.checkBox_4, self.checkBox_5, self.checkBox_7]
         restaurant = [self.checkBox_9, self.checkBox_10, self.checkBox_11, self.checkBox_12, self.checkBox_13, self.checkBox_14, self.checkBox_15, self.checkBox_16]
-        #設定不能重複
         for cate in category:
             if cate.isChecked():
                 cate_input = cate.text()
-        for res in restaurant:
-            if res.isChecked():
-                res_input = res.text()
-        data=db.child("restaurant").get()
+                print(cate_input)
+        if self.checkBox.isChecked():
+            for res in restaurant:
+                if res.isChecked():
+                    res_input = res.text()
+                    print(res_input)
+        '''data=db.child("restaurant").get()
         for rest in data.each():
             if cate_input == db.child("restaurant").child(rest.key()).child("tag1").get().val() and res_input == db.child("restaurant").child(rest.key()).child("tag2").get().val():
                 recom_rest.Resultwindow.restName=db.child("restaurant").child(rest.key()).child("name").get().val()
                 recom_rest_info.InfoWindow.restName=rest.key()
                 self.c=recom_rest.Resultwindow()
                 self.c.show()
-                df.add_user(self.userid,rest.key(),"5")
+                df.add_user(self.userid,rest.key(),"5")'''
     def toMain(self):
         self.close()
         self.c=main.Mainwindow()
         self.c.show()
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
